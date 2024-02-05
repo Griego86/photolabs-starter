613b1efebf6742d236186fa9a9c7359872bc3931
Griego86 <dimi1086@hotmail.com>
2024-02-04 19:44:26 -0800

    Modified PhotoListItem component styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,11 +7,15 @@ const PhotoListItem = (props) => {
   return (
     <div className="photo-list__item" key={id}>
       <img src={imageSource} className="photo-list__image"></img>
-      <div className="photo-list__user-details">
-      <img src={profile} className="photo-list__user-profile"></img>
-        <p className="photo-list__user-info">{username}</p>
+      <div className="photo-list__user-info photo-list__user-details">
+        <img src={profile} className="photo-list__user-profile"></img>
+        <div>
+          {username} 
+          <br>
+          </br>
+          <span className="photo-list__user-location">{location.city}, {location.country}</span>
+        </div>
       </div>
-        <p className="photo-list__user-location">{location.city}, {location.country}</p>
     </div>
   )
 };
