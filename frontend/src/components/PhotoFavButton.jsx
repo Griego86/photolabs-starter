51df8f9509e3f9878600d517054e37acd3f8de03
Griego86 <dimi1086@hotmail.com>
2024-02-10 09:54:17 -0800

    Added photo details model route to app.jsx

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,31 +1,31 @@
-import React, { useCallback, useState } from 'react';
+import React from "react";
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 
 function PhotoFavButton(props) {
-  const {favorites, setFavorites, id} = props;
-  const [selected, setSelected] = useState(false);
-  
+  const {favorites, setFavorites, id, setFavSelected, favSelected} = props;
+
   const handleClick = () => {
     setFavorites(prevFavorites => {
       if (prevFavorites.includes(id)) {
-        setSelected(false)
         // Remove the element if it exists
         return prevFavorites.filter(fav => fav !== id);
       } else {
-        setSelected(true);
         // Add the element if it doesn't exist
         return [...prevFavorites, id]
       }
-      })
-  };
+    })
+
+    setFavSelected(prevFavSelected => !prevFavSelected)
+    console.log(favSelected)
+  } 
 
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg" onClick={handleClick} >
-        <FavIcon selected={selected}/>
+        <FavIcon favSelected={favSelected}/>
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from 'react';
 
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from './PhotoFavButton';
@@ -8,9 +8,15 @@ const PhotoListItem = (props) => {
   const {favorites, setFavorites} = props;
   const id = props.photo.id;
 
+  const [favSelected, setFavSelected] = useState(false);
+
+  const handleClick = () => {
+
+  }
+
   return (
-    <div className="photo-list__item">
-      <PhotoFavButton favorites={favorites} setFavorites={setFavorites} id={id}/>
+    <div className="photo-list__item" onClick={handleClick}>
+      <PhotoFavButton setFavorites={setFavorites} id={id} setFavSelected={setFavSelected} favSelected={favSelected}/>
       <img src={urls.regular} className="photo-list__image"></img>
       <div className="photo-list__user-info photo-list__user-details">
         <img src={user.profile} className="photo-list__user-profile"></img>
